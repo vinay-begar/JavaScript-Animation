25d1169890246ff7137dd531386d4b74308aa6e8
Vinay Begar <vinaybbegar@gmail.com>
2025-10-18 15:35:37 +0530

    folder structure set properly

---
+++ 05 - GSAP ScrollTrigger/script.js
@@ -0,0 +1,44 @@
+gsap.to(".page1 .circle", {
+  rotate: 360,
+  scale: 0.5,
+  scrollTrigger: {
+    trigger: ".page1 .circle",
+    // markers:true,
+    // scroller hamesha body rahega
+    scroller: "body",
+    start: "top 0%",
+    end: "top 80%",
+    pin: true,
+    scrub: 5,
+  },
+});
+
+gsap.to(".page2 .circle", {
+  rotate: 360,
+  scale: 0.5,
+  scrollTrigger: {
+    trigger: ".page2 .circle",
+    // markers:true,
+    // scroller hamesha body rahega
+    scroller: "body",
+    start: "top 30%",
+    end: "top 80%",
+    pin: true,
+    scrub: 5,
+  },
+});
+
+gsap.to(".page3 .circle", {
+  rotate: 360,
+  scale: 0.5,
+  scrollTrigger: {
+    trigger: ".page3 .circle",
+    // markers:true,
+    // scroller hamesha body rahega
+    scroller: "body",
+    start: "top 50%",
+    end: "top 30%",
+    pin: true,
+    scrub: 5,
+  },
+});
